051390a4072fdfa6222c5877edf1dc9629d3bbe4
Antonio Vilela Pereira <sha1-99b95d901683f11ac96ebd76fd2bbc9bee4e1aa0@cern.ch>
2007-03-15 18:06:19 +0000

    simple print event analyzer

---
+++ test/PrintEvent.h
@@ -0,0 +1,38 @@
+#ifndef PRINT_EVENT
+#define PRINT_EVENT
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "CLHEP/HepMC/WeightContainer.h"
+#include "CLHEP/HepMC/GenEvent.h"
+#include "CLHEP/HepMC/GenParticle.h"
+
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+
+
+//
+// class decleration
+//
+
+class PrintEvent : public edm::EDAnalyzer {
+   public:
+      explicit PrintEvent(const edm::ParameterSet&);
+      ~PrintEvent();
+
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data ---------------------------
+      
+};
+
+#endif
