c3ce75ffe8d7fc5c9635994c09887a6e7181014c
John Marraffino <sha1-3565b4bb138dc66a8251e997feee5430e8526679@cern.ch>
2008-04-09 14:50:54 +0000

    Insert new and modified files needed to run Exhume as either a source or as a producer.

---
+++ test/ExhumeProducerAnalyzer.h
@@ -0,0 +1,49 @@
+#ifndef EXHUME_ANALYZER
+#define EXHUME_ANALYZER
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//#include "HepMC/WeightContainer.h"
+//#include "HepMC/GenEvent.h"
+//#include "HepMC/GenParticle.h"
+
+//#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+
+#include "TH1D.h"
+#include "TFile.h"
+
+//
+// class decleration
+//
+
+class ExhumeProducerAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit ExhumeProducerAnalyzer(const edm::ParameterSet&);
+      ~ExhumeProducerAnalyzer();
+
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data ---------------------------
+ 
+  std::string outputFilename;     
+  TH1D* hist_eta;
+  TH1D* hist_t1;
+  TH1D* hist_xigen1;
+  TH1D* hist_t2;
+  TH1D* hist_xigen2;	
+  TH1D* hist_sHat;
+  TH1D* hist_MX;
+
+};
+
+#endif
